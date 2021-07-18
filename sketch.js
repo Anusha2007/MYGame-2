d3608fc9e3861eebffdd9ca19b729b8ab1bf43d9
Anusha2007 <75532231+Anusha2007@users.noreply.github.com>
2021-07-18 17:03:07 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -5,19 +5,20 @@ const Engine=Matter.Engine;
 var myengine,myworld;
 var ground,player,backgroundImg1,backgroundImg2;
 var supplyImg,oxyTanksImg;
+var score=0, supplyGroup, oxyGroup;
 
 function preload(){
 backgroundImg1=loadImage("background 1.jpg");
 backgroundImg2=loadImage("background 2.jpg");
-astronautImg=loadImage("astronaut-2.png");
+astronautImg=loadImage("astronaut-3.jpg");
 supplyImg=loadImage("package.png");
 oxyTanksImg=loadImage("oxygen_tanks.png");
 }
 
 function setup(){
-createCanvas(displayWidth,displayHeight);
-myengine=Engine.create();
-myworld=myengine.world;
+createCanvas(1000,800);
+//myengine=Engine.create();
+//myworld=myengine.world;
 
 player=createSprite(200,400,30,40);
 player.addImage(astronautImg);
@@ -26,31 +27,74 @@ player.scale=0.6;
 //ground1= new Ground(600,530,1300,20);
 ground=createSprite(600,530,1300,20);
 ground.velocityX=-4;
-if (ground.x < 0){
-    ground.x = ground.width/2;
-  }
-//supply=new Supply(1000,Math.round(random(100,500)),40,40);
 
+//supply=new Supply(1000,Math.round(random(100,500)),40,40);
+//make groups for supply and oxygen
+supplyGroup=new Group();
+oxyGroup=new Group();
+
+//player.debug=true
+//player.setCollider("rectangle",0,0,20,mario.height)
 }
 
 function draw(){
-Engine.update(myengine);
+//Engine.update(myengine);
 background(backgroundImg1);
 
-getSupplies();
-getOxygen();
 //player.display();
 //ground1.display();
 //supply.display();
+
+if (ground.x < 0){
+    ground.x = ground.width/2;
+  }
+
+if(keyDown("up")){
+    player.y=player.y-10;
+}
+if(keyDown("down")){
+    player.y=player.y+10;
+}
+if(keyDown("right")){
+    player.x=player.x+10;
+}
+if(keyDown("left")){
+    player.x=player.x-10;
+}
+
+if(keyDown("space")){
+    player.y=player.y+20;
+}
+
+for(i=1;i<=supplyGroup.length;i++){
+    if(player.isTouching(supplyGroup.get(i))){
+        supplyGroup.get(i).destroy();
+        score=score+1;
+    }
+}
+
+for(i=1;i<=oxyGroup.length;i++){
+    if(player.isTouching(oxyGroup.get(i))){
+        oxyGroup.get(i).destroy();
+        score=score+1;
+    }
+}
+
+getSupplies();
+getOxygen();
 drawSprites();
+
+text("Score: "+score,700,100);
 }
 
 function getSupplies(){
     if(frameCount%150===0){
-        var supply=createSprite(1000,Math.round(random(100,500)),40,40);
+        var supply=createSprite(1000,Math.round(random(300,500)),40,40);
         supply.velocityX=-4;
         supply.addImage(supplyImg);
         supply.scale=0.3;
+        supplyGroup.add(supply);
+        supply.lifetime=250;
         }
 }
 
@@ -60,5 +104,7 @@ function getOxygen(){
         oxyTanks.velocityX=-2;
         oxyTanks.addImage(oxyTanksImg);
         oxyTanks.scale=0.3;
+        oxyGroup.add(oxyTanks);
+        oxyTanks.lifetime=250;
     }
 }
